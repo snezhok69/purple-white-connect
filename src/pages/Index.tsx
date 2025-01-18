d0b29d51586656a328ddd1b83494cf6bd8618b99
snezhok69_ <150421263+snezhok69@users.noreply.github.com>
2025-01-18 23:03:13 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -38,17 +38,7 @@ const Index = () => {
           <h2 className="text-2xl font-bold text-[#CD412B] mb-8 text-center">
             Наши Социальные Сети
           </h2>
-          
-        <SocialLinks />
-        <a
-          href="https://shop.bochkarust.space"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="social-button"
-        >
-          Наш магазин
-        </a>
-        
+          <SocialLinks />
         </div>
       </div>
     </div>
