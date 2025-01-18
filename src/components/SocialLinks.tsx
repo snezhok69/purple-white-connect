f3129420612bcd59c216a6bad3d0981a4a790d07
snezhok69_ <150421263+snezhok69@users.noreply.github.com>
2025-01-18 23:04:04 +0300

    Update SocialLinks.tsx

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -17,6 +17,11 @@ const SocialLinks = () => {
       url: "https://ds.bochkarust.space",
       className: "bg-[#2D1618] hover:bg-[#CD412B] border border-[#CD412B]/30",
     },
+    {
+      name: "Наш магазин",
+      url: "https://shop.bochkarust.space",
+      className: "bg-[#2D1618] hover:bg-[#CD412B] border border-[#CD412B]/30",
+    },
   ];
 
   return (
